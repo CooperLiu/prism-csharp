c7ee324592a939d9f6696c1a3d8f237fdd64fbb7
unknown <xushuai@SHOPEX-SH-A0114.office.shopex.cn>
2015-03-09 16:21:01 +0800

    实现消息推送

---
+++ Source/PrismNotify/Delivery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prism.PrismNotify
+{
+    //消息内容
+    public class Delivery
+    {
+        public string Key;
+        public string App;
+        public string RoutingKey;
+        public string ContentType;
+        public object Body;
+        public int Time;
+        public int Tag;
+    }
+}
