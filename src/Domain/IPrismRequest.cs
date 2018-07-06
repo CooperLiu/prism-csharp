ebfb82860a7684fc55799194988ea9317a7d3f64
Cooperliu <liuyankang@jk724.com>
2018-07-06 10:25:52 +0800

    fixed：调整Json.net版本

--- src/Domain/IPrismRequest.cs
+++ src/Domain/IPrismRequest.cs
@@ -14,6 +14,11 @@
         TRequestData Data { get; set; }
     }
 
+    /// <summary>
+    /// 请求商派系统接口基类
+    /// </summary>
+    /// <typeparam name="TRequestData"></typeparam>
+    /// <typeparam name="TResponseData"></typeparam>
     public abstract class PrismRequestBase<TRequestData, TResponseData> : IPrismRequest<TRequestData, TResponseData> where TRequestData : class, new()
     {
         public virtual string HttpMethod { get; set; } = "POST";
@@ -22,6 +27,11 @@
         public virtual TRequestData Data { get; set; }
     }
 
+    /// <summary>
+    /// 商派回调请求基类
+    /// </summary>
+    /// <typeparam name="TRequestData"></typeparam>
+    /// <typeparam name="TResponseData"></typeparam>
     public abstract class PrismWebhookRequestBase<TRequestData, TResponseData> : IPrismRequest<TRequestData, TResponseData> where TRequestData : class, new()
     {
         public virtual string HttpMethod { get; set; } = "POST";
--- src/Domain/StoreTradeUpdateRequest.cs
+++ src/Domain/StoreTradeUpdateRequest.cs
@@ -1,7 +1,8 @@
 ﻿namespace Prism.Domain
 {
     /// <summary>
-    /// store.trade.update 订单更新 支付单 支付变更都是通过订单更新接口完成    /// </summary>
+    /// store.trade.update 订单更新 支付单 支付变更都是通过订单更新接口完成
+    /// </summary>
     public class StoreTradeUpdateRequest : PrismRequestBase<StoreTradeUpdateRequestData, object>
     {
         public override string ApiMethod { get; set; } = "store.trade.update";
